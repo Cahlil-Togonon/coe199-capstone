9b1771854851b16acd166dd59590929a94a9e275
Cahlil Togonon <adrian.cahlil.eiz.togonon@eee.upd.edu.ph>
2025-06-22 16:29:33 +0800

    added error handling

--- ph_polygonize.py
+++ ph_polygonize.py
@@ -3,16 +3,19 @@ import json
 from rasterio.features import shapes
 
 def polygonize(threshold, date_time):
-    mask = None
-    with rasterio.Env():
-        with rasterio.open("./shapefiles/Philippines_Pollution_"+date_time+".tif") as src:
-        # with rasterio.open("./shapefiles/Philippines_Pollution_idw.tif") as src:
-            image = src.read(1) # first band
-            image = image.astype('int16')
-            geoms = [{'type':'Feature','properties': {'id': f'aqi_{int(v)}', 'AQI': int(v)}, 'geometry': s} for s,v in shapes(image, mask=mask, transform=src.transform) if v <= 1000]
-    output_dict = {"type": "FeatureCollection", "name": "polygonized", "threshold": threshold, "crs": {"type": "name", "properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84", "date_time": date_time}}, "features": geoms}
-    json_output = json.dumps(output_dict)
-    with open("./temp/polygonized_"+date_time+".json", "w") as outfile:
-        outfile.write(json_output)
-    with open("../express-leaflet/public/polygonized.json", "w") as outfile:  # point this to your leaflet+valhalla github folder
-        outfile.write(json_output)
+    try:
+        mask = None
+        with rasterio.Env():
+            with rasterio.open("./shapefiles/Philippines_Pollution_"+date_time+".tif") as src:
+            # with rasterio.open("./shapefiles/Philippines_Pollution_idw.tif") as src:
+                image = src.read(1) # first band
+                image = image.astype('int16')
+                geoms = [{'type':'Feature','properties': {'id': f'aqi_{int(v)}', 'AQI': int(v)}, 'geometry': s} for s,v in shapes(image, mask=mask, transform=src.transform) if v <= 1000]
+        output_dict = {"type": "FeatureCollection", "name": "polygonized", "threshold": threshold, "crs": {"type": "name", "properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84", "date_time": date_time}}, "features": geoms}
+        json_output = json.dumps(output_dict)
+        with open("./temp/polygonized_"+date_time+".json", "w") as outfile:
+            outfile.write(json_output)
+        with open("../express-leaflet/public/polygonized.json", "w") as outfile:  # point this to your leaflet+valhalla github folder
+            outfile.write(json_output)
+    except Exception as err:
+        print (err)
